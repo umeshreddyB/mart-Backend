4f690cbbcd814e1bc849ed28037dc387d19b4bdf
umeshreddy <umeshhoney7@gmail.com>
2025-08-16 11:23:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken'
+
+import cors from 'cors';
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+const MONGO_URL = process.env.MONGO_URL;
+
+
+
+mongoose.connect(MONGO_URL)
+.then(() => {
+  console.log('Connected to MongoDB'); 
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);})
+
+const userSchem= new mongoose.Schema({
+  name : string,
+  email : string,
+  place : string
+})
+const usern= mongoose.model("User",userSchem);
+
+app.post("/login", async(req,res)=>{
+    const {username,email}=req.body;
+    const result = await usern.find({username,email})
+    if(result.length>0){
+        const payload={
+          username,
+          email
+        }
+      const token = jwt.sign(payload,"ABC") 
+      res.status(200).json({token})
+    }
+    else{
+      res.send("user invaild")
+    }
+})
+
+
+
+app.post("/register",async(req,res)=>{
+
+    const {username,email,place}=req.body;
+    const result = await usern.find({username,email})
+      if(result.length>0){
+        res.status(500).send("user already register")
+      }
+      else{
+        const newuser = new usern({name,email,place})
+        newuser.save();
+          const payload={
+              username,
+              email,
+              place,
+            }
+          const token = jwt.sign(payload,"ABC") 
+          res.status(200).json({token})
+      }
+})
+
+
+
+
+
+
